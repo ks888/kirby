b15eea0029a3f1696846443e64bd3dfd1efa7cdd
Kishin Yagami <k.yagami.suou@gmail.com>
2015-06-27 19:43:38 +0900

    Add callback plugin to hook a task exec

---
+++ callback_plugins/analyze_coverage.py
@@ -0,0 +1,10 @@
+
+class CallbackModule(object):
+    """Plugin for analyzing task coverage"""
+
+    def __init__(self):
+        pass
+
+    def playbook_on_task_start(self, name, is_conditional):
+        print name
+        print is_conditional
