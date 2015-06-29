0065ad176313717d320674654ca4df21cad38daf
Kishin Yagami <k.yagami.suou@gmail.com>
2015-06-30 00:00:04 +0900

    Impl serverspec run (successful case only)

--- kirby/serverspec_runner.py
+++ kirby/serverspec_runner.py
@@ -1,11 +1,25 @@
 
+import os
+import re
+import subprocess
+
 
 class ServerspecRunner(object):
     """Run serverspec command and retrieve its results"""
+    pattern = re.compile(r'(\d+) examples, (\d+) failures')
 
     def __init__(self, serverspec_dir, serverspec_cmd):
         self.serverspec_dir = serverspec_dir
         self.serverspec_cmd = serverspec_cmd
 
     def run(self):
-        return None
+        orig_dir = os.getcwd()
+        os.chdir(self.serverspec_dir)
+        cmd_result = subprocess.check_output(self.serverspec_cmd)
+        os.chdir(orig_dir)
+
+        match_result = ServerspecRunner.pattern.search(cmd_result)
+        num_test = int(match_result.group(1))
+        num_failed_test = int(match_result.group(2))
+
+        return (num_test, num_failed_test)
--- tests/unit/serverspec_runner_test.py
+++ tests/unit/serverspec_runner_test.py
@@ -1,5 +1,7 @@
 
+from mock import patch
 import unittest
+
 from kirby.serverspec_runner import ServerspecRunner
 
 
@@ -7,5 +9,17 @@ class ServerspecRunnerTest(unittest.TestCase):
     def setUp(self):
         self.runner = ServerspecRunner('.', '')
 
-    def test_run(self):
-        pass
+    @patch('subprocess.check_output', return_value="2 examples, 0 failures")
+    def test_run_successful(self, mock):
+        result = self.runner.run()
+        self.assertTupleEqual(result, (2, 0))
+
+    @patch('subprocess.check_output', return_value="2 examples, 1 failures")
+    def test_run_failure(self, mock):
+        result = self.runner.run()
+        self.assertTupleEqual(result, (2, 1))
+
+    @patch('subprocess.check_output', return_value="100 examples, 99 failures")
+    def test_run_100tests(self, mock):
+        result = self.runner.run()
+        self.assertTupleEqual(result, (100, 99))
