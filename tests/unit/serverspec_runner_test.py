9068f5947ffe69d67dc938bdd0745e6efe0480fc
Kishin Yagami <k.yagami.suou@gmail.com>
2015-07-26 18:07:35 +0900

    Refatoring Tests (serverspec runner)

--- tests/unit/serverspec_runner_test.py
+++ tests/unit/serverspec_runner_test.py
@@ -13,31 +13,31 @@ class ServerspecRunnerTest(unittest.TestCase):
     def setUp(self):
         self.runner = ServerspecRunner('.', '')
 
-    @patch('subprocess.check_output', return_value="2 examples, 0 failures")
-    def test_run_successful(self, mock):
+    @patch('subprocess.check_output', return_value="1 example, 0 failures")
+    def testRun_NoFailedTest_NumTestsIsSet(self, mock):
         result = self.runner.run()
-        self.assertTupleEqual(result, (2, 0, []))
+        self.assertTupleEqual(result, (1, 0, []))
 
     @patch('subprocess.check_output',
            side_effect=subprocess.CalledProcessError(1, '', output="1 example, 1 failure\nrspec ..."))
-    def test_run_failure(self, mock):
+    def testRun_Has1FailedTest_FailedTestIsSet(self, mock):
         result = self.runner.run()
         self.assertTupleEqual(result, (1, 1, ['rspec ...']))
 
     @patch('subprocess.check_output',
+           side_effect=subprocess.CalledProcessError(1, '', output="2 examples, 2 failures\nrspec ... rspec ...\nrspec ..."))
+    def testRun_Has2FailedTests_FailedTestIsSet(self, mock):
+        result = self.runner.run()
+        self.assertTupleEqual(result, (2, 2, ['rspec ... rspec ...', 'rspec ...']))
+
+    @patch('subprocess.check_output',
            side_effect=subprocess.CalledProcessError(1, '', output=""))
-    def test_run_empty_output(self, mock):
+    def testRun_InvalidOutput_ReturnNone(self, mock):
         result = self.runner.run()
         self.assertIsNone(result)
 
     @patch('subprocess.check_output',
            side_effect=subprocess.CalledProcessError(1, '', output="100 examples, 99 failures"))
-    def test_run_100tests(self, mock):
+    def testRun_LargeNumOfTests_CaptureCorrectly(self, mock):
         result = self.runner.run()
         self.assertTupleEqual(result, (100, 99, []))
-
-    @patch('subprocess.check_output',
-           side_effect=subprocess.CalledProcessError(1, '', output="2 examples, 2 failures\nrspec ... rspec ...\nrspec ..."))
-    def test_run_2_failed_tests(self, mock):
-        result = self.runner.run()
-        self.assertTupleEqual(result, (2, 2, ['rspec ... rspec ...', 'rspec ...']))
